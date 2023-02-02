e96aa618ea42d501f560e651a037ee5ae0a65204
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-02-02 11:52:24 +0200

    Made task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsRef = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+})
+console.log(ingredientsRef);
+
+const ingredientsListRef = document.querySelector('#ingredients');
+ingredientsListRef.append(...ingredientsRef);
+console.log(ingredientsListRef);
