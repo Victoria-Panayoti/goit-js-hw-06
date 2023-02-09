d8ca4eed92ca2f631f28216ae9d734e479758538
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-02-09 07:48:12 +0200

    Made task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const refs = {
+    input: document.querySelector('#name-input'),
+    span: document.querySelector('#name-output'),
+}
+refs.input.addEventListener('input', onInputEnter);
+
+function onInputEnter(enter) {
+    enter.preventDefault();
+    refs.span.textContent = enter.currentTarget.value;
+}
