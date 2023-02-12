3fc67d89c3a94085010034ad8448b1773a7262ec
Victoria Panajoti <112144156+Victoria-Panayoti@users.noreply.github.com>
2023-02-12 10:56:25 +0200

    made task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorChangingBtn = document.querySelector('.change-color');
+const changeableBody = document.querySelector('body');
+const colorName = document.querySelector('.color');
+colorChangingBtn.addEventListener('click', onColorChangingBtnClick);
+
+function onColorChangingBtnClick() {
+  // changeableBody.style.backgroundColor = getRandomHexColor();
+  // colorName.textContent = `${getRandomHexColor()}`;
+  // colorName.textContent = `${getRandomHexColor}`;
+  const background = getRandomHexColor();
+  changeableBody.style.background = background;
+  colorName.textContent = `${background}`;
+  console.log(changeableBody);
+}
